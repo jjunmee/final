5ab9d56b275a3ecad78cc4dd45d87dccbf75e709
jin <jin@localhost>
2018-10-22 21:13:48 +0900

    form setting

---
+++ netfilx/src/main/java/com/jhta/netflix/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.jhta.netflix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
+	public String loginForm() {
+		return ".user.login";
+	}
+}
---
+++ netfilx/src/main/webapp/WEB-INF/views/user/login.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<div>
+	<h1>로그인페이지</h1>
+	<form method="POST" action="<c:url value='/login' />" >
+		
+	</form>
+</div>
