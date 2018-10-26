990db9dd5ad966305028784965668eb8cdd4040f
juhee <dkrkekjh@gmail.com>
2018-10-26 17:58:17 +0900

    jh

--- netfilx/src/main/java/com/jhta/netflix/survey/controller/SurveyController.java
+++ netfilx/src/main/java/com/jhta/netflix/survey/controller/SurveyController.java
@@ -1,6 +1,8 @@
 package com.jhta.netflix.survey.controller;
 
 import java.io.File;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,9 +32,11 @@ public class SurveyController {
 	@RequestMapping(value="/survey/surveyInsert",method=RequestMethod.POST)
 	public String survey(HttpServletRequest request,HttpServletResponse response) {		
 		String surveyName=request.getParameter("surveyName");
-		
-		
-		
+		String surveyContent=request.getParameter("surveyContent");
+		String end=request.getParameter("surveyEnd");
+		Date surveyEnd=Date.valueOf(end);
+		SurveyVo surveyVo=new SurveyVo(0, null, surveyName, surveyContent, null, surveyEnd);
+		service.insert();
 		
 		
 		System.out.println(surveyName);
--- netfilx/src/main/java/com/jhta/netflix/survey/service/SurveyService.java
+++ netfilx/src/main/java/com/jhta/netflix/survey/service/SurveyService.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jhta.netflix.survey.dao.SurveyDao;
+import com.jhta.netflix.survey.vo.SurveyVo;
 
 @Service
 public class SurveyService {
 	@Autowired private SurveyDao dao;
+	
+	public int insert(SurveyVo vo) {
+		
+	}
 	
 }
