3ab369b5b626f717ae30add252ae840d2eb68074
jin <jin@localhost>
2018-11-01 17:03:20 +0900

    으캬캬캬캬캬

--- netfilx/src/main/java/com/jhta/netflix/user/controller/testCon.java
+++ netfilx/src/main/java/com/jhta/netflix/user/controller/testCon.java
@@ -2,12 +2,8 @@ package com.jhta.netflix.user.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
-import org.springframework.social.oauth2.GrantType;
-import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,27 +19,12 @@ public class testCon {
 	@Autowired
 	private OAuth2Parameters googleOAuth2Parameters;
 
-	// 로그인 첫 화면 요청 메소드
-	@RequestMapping(value = "/login/google", method = { RequestMethod.GET, RequestMethod.POST })
-	public String login(Model model, HttpSession session) {
-
-		/* 구글code 발행 */
-		OAuth2Operations oauthOperations = googleConnectionFactory.getOAuthOperations();
-		String url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, googleOAuth2Parameters);
-
-		System.out.println("구글:" + url);
-
-		model.addAttribute("google_url", url);
-
-		/* 생성한 인증 URL을 View로 전달 */
-		return "login";
-	}
 
 	// 구글 Callback호출 메소드
-	@RequestMapping(value = "/oauth2callback", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = "/user/google", method = { RequestMethod.GET, RequestMethod.POST })
 	public String googleCallback(Model model, @RequestParam String code) throws IOException {
 		System.out.println("여기는 googleCallback");
 
-		return "googleSuccess";
+		return ".main";
 	}
 }
