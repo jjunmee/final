02d804b053576e1e5e36def6b5f6aeb090f54b08
jin <jin@localhost>
2018-11-02 17:18:49 +0900

    구글 로그인 회원가입 완료

---
+++ netfilx/src/main/java/com/jhta/netflix/lib/UserStatus.java
@@ -0,0 +1,10 @@
+package com.jhta.netflix.lib;
+
+public class UserStatus {
+	public static final int DEFAULT_USER = 0;//일반사용자
+	public static final int ADMIN_USER = 1;//관리자
+	public static final int TRUE_USER = 2;//본인인증자
+	public static final int AGE_USER = 3;//성인인증자
+	public static final int PERFACT_USER = 4;//2+3번
+	public static final int FALSE_USER = 9;//탈퇴회원
+}
