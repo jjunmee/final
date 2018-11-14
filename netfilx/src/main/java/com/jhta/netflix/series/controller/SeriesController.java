4f0e450c444a8a508faa4d1ecd54a62ded1661f5
Kang-minsung <zaqwesdxc@naver.com>
2018-11-14 20:50:15 +0900

    ing

--- netfilx/src/main/java/com/jhta/netflix/series/controller/SeriesController.java
+++ netfilx/src/main/java/com/jhta/netflix/series/controller/SeriesController.java
@@ -1,26 +1,56 @@
 package com.jhta.netflix.series.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.jhta.netflix.lib.PageUtil;
 import com.jhta.netflix.series.service.SeriesService;
 import com.jhta.netflix.series.vo.SeriesVo;
+import com.jhta.netflix.staff.vo.StaffVo;
 
 @Controller
 public class SeriesController {
 	@Autowired
 	private SeriesService service;
 	
+	@RequestMapping(value="/series/list")
+	public String list(Model model,@RequestParam(value="pageNum",defaultValue="1")int pageNum,
+			String keyword) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int totalRowCount = service.count(keyword);
+		if(keyword != null && !keyword.trim().equals("")) {
+			map.put("keyword", keyword);
+			model.addAttribute("keyword", keyword);
+		}
+		if(totalRowCount != 0) {
+			PageUtil pageUtil = new PageUtil(pageNum, totalRowCount, 10, 10);
+			if(pageNum > pageUtil.getTotalPageCount()) {
+				pageUtil = new PageUtil(pageUtil.getTotalPageCount(), totalRowCount, 10, 10);
+			}
+			map.put("startRow", pageUtil.getMysqlStartRow());
+			map.put("rowBlockCount", pageUtil.getRowBlockCount());
+			List<SeriesVo> list = service.list(map);
+			model.addAttribute("list", list);
+			model.addAttribute("pageUtil", pageUtil);
+		}
+		return ".series.list";
+	}
 	@RequestMapping(value="/content/serieslist",produces="application/json;charset=utf-8")
 	@ResponseBody
 	public String stafflist(String name) {
-		List<SeriesVo> list = service.list(name);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("keyword", name);
+		List<SeriesVo> list = service.list(map);
 		JSONArray arr = new JSONArray();
 		for(SeriesVo vo : list) {
 			JSONObject json = new JSONObject();
