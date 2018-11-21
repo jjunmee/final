67085773e15fb11afb191c933264c30cdf79db53
jin <jin@LAPTOP-KBNQ1L5M>
2018-11-21 14:18:01 +0900

    admin

---
+++ netfilx/src/main/java/com/jhta/netflix/AdminIntercepter.java
@@ -0,0 +1,39 @@
+package com.jhta.netflix;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AdminIntercepter implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+			HttpSession session = request.getSession();
+			int n = (Integer)session.getAttribute("sts");
+			if(n!=1) {
+				response.sendRedirect(session.getServletContext().getContextPath()+"/main");
+				return false;
+			}
+			return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
